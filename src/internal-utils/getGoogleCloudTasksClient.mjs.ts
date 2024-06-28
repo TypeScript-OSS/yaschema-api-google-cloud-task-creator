ae19bbd639be3ccfb645b17ce45a58868100315a
Brian Westphal <brian.westphal@panchaininc.com>
2024-06-28 10:31:26 +0800

    v1.6.3: - Fixed esm issue with lodash usage

--- src/internal-utils/getGoogleCloudTasksClient.mjs.ts
+++ src/internal-utils/getGoogleCloudTasksClient.mjs.ts
@@ -1,4 +1,4 @@
 import { CloudTasksClient } from '@google-cloud/tasks';
-import { once } from 'lodash';
+import { once } from 'lodash-es';
 
 export const getGoogleCloudTasksClient = once(() => new CloudTasksClient());
